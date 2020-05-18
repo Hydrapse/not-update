a5fc04b3eff7aefc4fc70c5b68999153705779f8
Hydrapse <1041422509@qq.com>
2020-05-18 11:04:47 +0800

    之前目录用户结构修改错误, 现在更正

--- .temp/components/index.js
+++
@@ -1,5 +0,0 @@
-// import PostCard from './postcard'
-// import PostForm from './postform'
-import SubTitle from "./subtitle/index";
-
-export { SubTitle };
--- src/components/index.jsx
+++
@@ -1,5 +0,0 @@
-// import PostCard from './postcard'
-// import PostForm from './postform'
-import SubTitle from './subtitle'
-
-export { SubTitle }
